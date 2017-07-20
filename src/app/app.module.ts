37f66bbccd2371f21f79ced996a548b6d9976d2c
Pakinam <pakinam_banna@hotmail.com>
2017-07-20 16:28:27 +0200

    add bootstrap library to project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { RightComponent } from './right/right.component';
+import { MiddleComponent } from './middle/middle.component';
+import { LeftComponent } from './left/left.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RightComponent,
+    MiddleComponent,
+    LeftComponent
   ],
   imports: [
     BrowserModule
