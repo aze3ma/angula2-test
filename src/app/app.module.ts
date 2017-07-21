ece9338276d90b6f1eb1c60f3e17300a779a28f1
Pakinam <pakinam_banna@hotmail.com>
2017-07-21 22:18:13 +0200

    test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,17 @@ import { AppComponent } from './app.component';
 import { RightComponent } from './right/right.component';
 import { MiddleComponent } from './middle/middle.component';
 import { LeftComponent } from './left/left.component';
+import { TopComponent } from './shared/top/top.component';
+import { SideComponent } from './shared/side/side.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     RightComponent,
     MiddleComponent,
-    LeftComponent
+    LeftComponent,
+    TopComponent,
+    SideComponent
   ],
   imports: [
     BrowserModule
