0e74fd170015479d606f2a12cb786e850c890407
Pakinam <pakinam_banna@hotmail.com>
2017-07-23 20:33:23 +0200

    cleanups

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,9 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-// import { RightComponent } from './right/right.component';
-// import { LeftComponent } from './left/left.component';
-// import { MiddleComponent } from './middle/middle.component';
-// import { TopComponent } from './shared/top/top.component';
-// import { SideComponent } from './shared/side/side.component';
 
 @Component({
   selector: 'app-main',
